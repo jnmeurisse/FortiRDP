9d9058034c3f1f681be81ec4f123e870f1fd7b30
jnmeurisse <jnmeurissegmail.com>
2022-02-19 16:59:11 +0100

    Initial commit

---
+++ src/http/Cookies.cpp
@@ -0,0 +1,98 @@
+/*!
+* This file is part of FortiRDP
+*
+* Copyright (C) 2022 Jean-Noel Meurisse
+* SPDX-License-Identifier: Apache-2.0
+*
+*/
+#include "http/Cookies.h"
+#include "tools/StrUtil.h"
+
+namespace http {
+
+	Cookies::Cookies()
+	{
+	}
+
+
+	Cookies::Cookies(const Cookies& cookies, const http::Url& url)
+	{
+		const std::string& valid_path = tools::trim(url.get_path());
+
+		for (auto it = cookies.cbegin(); it != cookies.cend(); it++) {
+			const Cookie cookie = it->second;
+			const std::string path = tools::trim(cookie.get_path());
+
+			if (path.size() == 0 || valid_path.compare(path) == 0) {
+				// cookie without path or same path
+				set(it->first, cookie);
+			}
+		}
+	}
+
+
+	Cookies::~Cookies()
+	{
+		clear();
+	}
+
+
+	void Cookies::clear()
+	{
+		_cookies.clear();
+	}
+
+
+	const Cookies& Cookies::add(const Cookies cookies)
+	{
+		for (auto it = cookies.cbegin(); it != cookies.cend(); it++) {
+			set(it->second.get_name(), it->second);
+		}
+
+		return *this;
+	}
+
+
+	const Cookies& Cookies::set(const std::string& name, const Cookie& value)
+	{
+		// check if the name exists in this collection. 
+		auto it = _cookies.find(name);
+
+		if (it != _cookies.end()) {
+			// If yes, update the value
+			it->second = value;
+		}
+		else {
+			// If no, insert the name-value pair
+			_cookies.insert(std::pair<const std::string, Cookie>(name, value));
+		}
+
+		return *this;
+	}
+
+
+	const Cookie& Cookies::get(const std::string& name) const
+	{
+		return _cookies.at(name);
+	}
+
+
+	bool Cookies::exists(const std::string& name) const
+	{
+		return _cookies.find(name) != _cookies.end();
+	}
+
+
+	tools::obfstring Cookies::to_header() const
+	{
+		tools::obfstring buffer;
+
+		// Create the Set-Cookie header
+		for (auto it = _cookies.cbegin(); it != _cookies.cend(); it++) {
+			buffer.append(it->second.to_header());
+			buffer.append("; ");
+		}
+
+		return buffer.size() > 0 ? buffer.substr(0, buffer.size() - 2) : tools::obfstring();
+	}
+}
---
+++ src/http/Cookies.h
@@ -0,0 +1,92 @@
+/*!
+* This file is part of FortiRDP
+*
+* Copyright (C) 2022 Jean-Noel Meurisse
+* SPDX-License-Identifier: Apache-2.0
+*
+*/
+#pragma once
+
+#include <string>
+#include <map>
+#include "tools/ByteBuffer.h"
+#include "tools/ObfuscatedString.h"
+#include "http/Url.h"
+#include "http/Cookie.h"
+
+namespace http {
+
+	/**
+	* A collection of cookies.
+	*/
+	class Cookies
+	{
+	public:
+		using cookiemap = std::map<const std::string, Cookie> ;
+		using const_iterator = cookiemap::const_iterator ;
+
+		/* Allocates a cookies collection
+		*/
+		explicit Cookies();
+
+		/* Clones a cookies collection associated with a given url. 
+		*/
+		explicit Cookies(const Cookies& cookies, const http::Url& url);
+
+		/* Destroys this cookies collection
+		*/
+		~Cookies();
+
+		/* Clears this cookies collection
+		*/
+		void clear();
+
+		/* Copies all cookies from the specified collection to this collection.
+		 * Existing cookies could be replaced during this operation.
+		 *
+		 * @param cookies The collection of cookies to be copied in this collection
+		*/
+		const Cookies& add(const Cookies cookies);
+
+		/* Sets the cookie with the specified name
+		 *
+		 * @param name The name of the cookie
+		 * @param value The value of the cookie
+		*/
+		const Cookies& set(const std::string& name, const Cookie& value);
+
+		/* Gets the cookie with the specified name. The function throws
+		 * an out_of_range exception if the cookie name does not exist.
+		 *
+		 * @param name The name of the cookie to be retrieved
+		 * @return A reference to the cookie
+		*/
+		const Cookie& get(const std::string& name) const;
+
+		/* Returns true if the cookie with the specified name exits.
+		*/
+		bool exists(const std::string& name) const;
+
+		/* Returns the number of cookies in this collection
+		*/
+		inline size_t size() const { return _cookies.size(); }
+
+		/* Constructs a string representation of the cookies collection.
+		 * The string format is compatible with Netscape specification and
+		 * can be added to a Cookie header
+		*/
+		tools::obfstring to_header() const;
+
+		/* Returns an iterator referring to the first element in the collection
+		*/
+		inline const_iterator cbegin() const { return _cookies.cbegin(); }
+
+		/* Returns an iterator referring to the last element in the collection
+		*/
+		inline const_iterator cend() const { return _cookies.cend(); }
+
+	private:
+		cookiemap _cookies;
+	};
+
+}
