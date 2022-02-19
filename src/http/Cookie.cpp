615883d19d5cea2cd872d5a203c5645f553f0ec2
jnmeurisse <jnmeurissegmail.com>
2022-02-19 16:46:16 +0100

    Initial commit

---
+++ src/http/Cookie.cpp
@@ -0,0 +1,112 @@
+/*!
+* This file is part of FortiRDP
+*
+* Copyright (C) 2022 Jean-Noel Meurisse
+* SPDX-License-Identifier: Apache-2.0
+*
+*/
+#include "http/Cookie.h"
+#include "tools/StrUtil.h"
+
+namespace http {
+
+	Cookie::Cookie(const tools::obfstring value)
+	{
+		if (!parse(value)) {
+			throw new CookieError("Invalid cookie value:" + value.uncrypt());
+		}
+	}
+
+
+	Cookie::~Cookie()
+	{
+	}
+
+
+	Cookie& Cookie::operator= (const Cookie& other)
+	{
+		if (this != &other) {
+			_name = other.get_name();
+			_value = other.get_value();
+			_domain = other.get_domain();
+			_path = other.get_path();
+			_expires = other.get_expires();
+			_secure = other.is_secure();
+			_http_only = other.is_http_only();
+		}
+
+		return *this;
+	}
+
+
+	tools::obfstring Cookie::to_header() const
+	{
+		return tools::obfstring(_name + "=").append(_value);
+	}
+
+
+	bool Cookie::parse(const tools::obfstring& value)
+	{
+		bool rc = false;
+		std::vector<tools::obfstring> parts;
+
+		// Split the cookie string. There should always have one cookie-pair pair.
+		// This pair contains the name and the value according
+		// to the syntax : 
+		//    cookie-pair *( ";" SP cookie-av )
+		//    cookie-pair       = cookie-name "=" cookie-value
+		if (tools::split(value, ';', parts) > 0) {
+			const tools::obfstring cookie_pair = parts[0];
+
+			// split cookie-pair 
+			std::string::size_type pos = cookie_pair.find('=');
+			if (pos != std::string::npos) {
+				_name = cookie_pair.substr(0, pos).uncrypt();
+				_value = tools::obfstring(cookie_pair.substr(pos + 1, std::string::npos));
+
+				rc = true;
+			}
+			else {
+				// invalid cookie
+				rc = false;
+			}
+
+			if (rc && parts.size() > 1) {
+				for (size_t idx = 1; idx < parts.size(); idx++) {
+					// get next attribute-value
+					const std::string cookie_av = tools::trimleft(parts[idx].uncrypt());
+					if (cookie_av.size() > 0) {
+						// extract attribute name and value
+						std::string::size_type pos = cookie_av.find('=');
+
+						if (pos != std::string::npos) {
+							const std::string attribute_name = cookie_av.substr(0, pos);
+							const std::string attribute_value = cookie_av.substr(pos + 1, std::string::npos);
+
+							if (tools::iequal(attribute_name, "domain")) {
+								_domain = attribute_value;
+							}
+							else if (tools::iequal(attribute_name, "path")) {
+								_path = attribute_value;
+							}
+							else if (tools::iequal(attribute_name, "expires")) {
+								_expires = attribute_value;
+							}
+						}
+						else if (tools::iequal(cookie_av, "secure")) {
+							_secure = true;
+						}
+						else if (tools::iequal(cookie_av, "httponly")) {
+							_http_only = true;
+						}
+					}
+				}
+			}
+		}
+
+		return rc;
+	}
+
+}
+
+
---
+++ src/http/Cookie.h
@@ -0,0 +1,85 @@
+/*!
+* This file is part of FortiRDP
+*
+* Copyright (C) 2022 Jean-Noel Meurisse
+* SPDX-License-Identifier: Apache-2.0
+*
+*/
+#pragma once
+
+#include <string>
+#include "http/CookieError.h"
+#include "tools/ObfuscatedString.h"
+
+namespace http {
+
+	/**
+	* Stores an HTTP Cookie and Set-Cookie header fields.
+	* (See rfc6265)
+	*/
+	class Cookie
+	{
+	public:
+		/* Allocates a cookie from a raw value. This constructor throws
+		 * a CookieError exception if the cookie can not be decoded according
+		 * to rfc6265.
+		*/
+		explicit Cookie(const tools::obfstring value);
+
+		/* Destroys this cookie
+		*/
+		~Cookie();
+
+		/* Assigns this cookie
+		*/
+		Cookie& operator= (const Cookie& other);
+
+		/* Returns an obfuscated string representation of this cookie.
+		 * The string format is compatible with Netscape specification.
+		*/
+		tools::obfstring to_header() const;
+
+		/* Returns the cookie name 
+		*/
+		inline const std::string& get_name() const noexcept { return _name; }
+
+		/* Returns the cookie value as an obfuscated string
+		*/
+		inline const tools::obfstring& get_value() const noexcept { return _value; }
+
+		/* Returns the cookie domain attribute
+		*/
+		inline const std::string& get_domain() const noexcept { return _domain; }
+
+		/* Returns the cookie path attribute
+		*/
+		inline const std::string& get_path() const noexcept{ return _path; }
+
+		/* Returns the cookie expires attribute
+		*/
+		inline const std::string& get_expires() const noexcept { return _expires; }
+
+		/* Returns true if the cookie secure attribute defined
+		*/
+		inline const bool is_secure() const noexcept { return _secure; }
+
+		/* Returns true if the cookie attribute httponly is defined
+		*/
+		inline const bool is_http_only() const noexcept { return _http_only; }
+
+	private:
+		// Parsed cookie 
+		std::string _name;
+		tools::obfstring _value;
+		std::string _domain;
+		std::string _path;
+		std::string _expires;
+		bool _secure = false;
+		bool _http_only = false;
+
+		/* Parse a cookie into attributes
+		*/
+		bool parse(const tools::obfstring& value);
+	};
+
+}
