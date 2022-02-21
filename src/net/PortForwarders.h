bea790afe43915f9816a99e95f2b88fb87b0227e
jnmeurisse <jnmeurissegmail.com>
2022-02-21 22:44:39 +0100

    Improve comments

--- src/net/PortForwarders.h
+++ src/net/PortForwarders.h
@@ -18,27 +18,30 @@ namespace net {
 	class PortForwarders : public std::list<PortForwarder*>
 	{
 	public:
+		/* Allocates an empty forwarder list.
+		*/
 		PortForwarders();
+
+		/* Delete all existing forwarders in that list.
+		*/
 		~PortForwarders();
 
 		/* Delete all port forwarders from the list in the given state.
 		*  Returns the number of deleted forwarders.
 		*/
-		int delete_having_state(state_check_cb check);
-
+		int delete_having_state(state_check_cb check_cb);
 
 		/* Disconnects all port forwarders.
-		*  Returns the number of deleted forwarders.
+		*  Returns the number of disconnected forwarders.
 		*/
 		int abort_all();
 
-		/*
+		/* Returns true if at least one port forwarder is trying to connect.
 		*/
 		bool has_connecting_forwarders() const;
 
 	private:
 		// a reference to the application logger
 		tools::Logger* const _logger;
-
 	};
 }
