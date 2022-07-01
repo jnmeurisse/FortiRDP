50188fd5a39051353a8f101e916fb2c9491e4b2d
jnmeurisse <jnmeurissegmail.com>
2022-07-01 07:49:59 +0200

    add namespace

--- src/net/DnsClient.cpp
+++ src/net/DnsClient.cpp
@@ -8,37 +8,39 @@
 #include <sstream>
 #include "net/DnsClient.h"
 
+namespace net {
 
-bool DnsClient::configured()
-{
-	return !ip4_addr_isany_val(*dns_getserver(0)) || !ip4_addr_isany_val(*dns_getserver(1));
-}
-
-
-std::string DnsClient::dns()
-{
-	std::ostringstream os;
-	const ip_addr_t *addr1 = dns_getserver(0);
-	const ip_addr_t *addr2 = dns_getserver(1);
-
-	if (!ip4_addr_isany_val(*addr1))
-		os << ip4addr_ntoa(addr1);
-
-	if (!ip4_addr_isany_val(*addr1) && !ip4_addr_cmp(addr1, addr2))
-		os << ip4addr_ntoa(addr2);
-
-	return os.str();
-}
+	bool DnsClient::configured()
+	{
+		return !ip4_addr_isany_val(*dns_getserver(0)) || !ip4_addr_isany_val(*dns_getserver(1));
+	}
 
 
-err_t DnsClient::query(const std::string& hostname, ip_addr_t& addr, dns_found_callback found_callback, void* callback_arg)
-{
-	return dns_gethostbyname_addrtype(hostname.c_str(), &addr, found_callback, callback_arg, LWIP_DNS_ADDRTYPE_IPV4);
-}
+	std::string DnsClient::dns()
+	{
+		std::ostringstream os;
+		const ip_addr_t *addr1 = dns_getserver(0);
+		const ip_addr_t *addr2 = dns_getserver(1);
+
+		if (!ip4_addr_isany_val(*addr1))
+			os << ip4addr_ntoa(addr1);
+
+		if (!ip4_addr_isany_val(*addr1) && !ip4_addr_cmp(addr1, addr2))
+			os << ip4addr_ntoa(addr2);
+
+		return os.str();
+	}
 
 
-std::string DnsClient::dns(int numdns)
-{
-	const ip_addr_t *addr = dns_getserver(numdns);
-	return ip4_addr_isany_val(*addr) ? std::string() : ip4addr_ntoa(addr);
+	err_t DnsClient::query(const std::string& hostname, ip_addr_t& addr, dns_found_callback found_callback, void* callback_arg)
+	{
+		return dns_gethostbyname_addrtype(hostname.c_str(), &addr, found_callback, callback_arg, LWIP_DNS_ADDRTYPE_IPV4);
+	}
+
+
+	std::string DnsClient::dns(int numdns)
+	{
+		const ip_addr_t *addr = dns_getserver(numdns);
+		return ip4_addr_isany_val(*addr) ? std::string() : ip4addr_ntoa(addr);
+	}
 }
--- src/net/DnsClient.h
+++ src/net/DnsClient.h
@@ -6,27 +6,45 @@
 *
 */
 #pragma once
+
 #include <string>
 #include "lwip/dns.h"
 #include "lwip/ip_addr.h"
 
+namespace net {
 
-class DnsClient
-{
-public:
-	DnsClient() = delete;
-	~DnsClient() = delete;
-
-	static bool configured();
-
-	static std::string dns();
-	static err_t query(const std::string& hostname, ip_addr_t& addr, dns_found_callback found_callback, void* callback_arg);
-
-private:
-	/*
+	/**
+	* DNSClient is a static class used to query the DNS server.  The DNS server address
+	* is obtained from the ppp server during the connection (see pppossl_connect). 
 	*/
-	static std::string dns(int numdns);
+	class DnsClient
+	{
+	public:
+		DnsClient() = delete;
+		~DnsClient() = delete;
 
+		/* Returns true if a DNS server address is available.
+		*/
+		static bool configured();
 
-};
+		/* Returns the DNS server address as a string 
+		*/
+		static std::string dns();
 
+		/* Gets the IP address of a given host name.
+		*
+		*  The function returns ERR_OK if the address is available in the dns client 
+		*  local cache and copies the address in addr.
+		*
+		*  The function returns ERR_INPROGRESS when the dns request is queued to
+		*  be sent to the dns server.  The found_callback is called later with the
+		*  result of the dns query.
+		*/
+		static err_t query(const std::string& hostname, ip_addr_t& addr, dns_found_callback found_callback, void* callback_arg);
+
+	private:
+		/*
+		*/
+		static std::string dns(int numdns);
+	};
+}
