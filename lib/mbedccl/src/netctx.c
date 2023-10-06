7f73ab088c9cee8ec2fdea493ae33b69488c7cf4
jnmeurisse <jnmeurissegmail.com>
2023-10-06 09:10:55 +0200

    update: change returned error code.
    Return mbed_err instead of windows error code in functions netctx_set_blocking and netctx_set_nodelay.
    Added a status code in the netctx_poll_status structure.

--- lib/mbedccl/src/netctx.c
+++ lib/mbedccl/src/netctx.c
@@ -30,13 +30,9 @@ void netctx_free(netctx* ctx)
 {
 	mbedtls_net_context* const net_context = (mbedtls_net_context*)ctx;
 
-	if (net_context) {
-		// close the connection if not yet done
-		mbedtls_net_close(net_context);
-
+	if (net_context)
 		// free the allocated memory
 		free(net_context);
-	}
 }
 
 
@@ -121,7 +117,7 @@ mbed_err netctx_accept(netctx* bind_ctx, netctx* accepting_ctx)
 }
 
 
-int netctx_set_blocking(netctx* ctx, int blocking)
+mbed_err netctx_set_blocking(netctx* ctx, int blocking)
 {
 	int rc = INVALID_SOCKET;
 	mbedtls_net_context* const net_context = (mbedtls_net_context*)ctx;
@@ -132,11 +128,11 @@ int netctx_set_blocking(netctx* ctx, int blocking)
 			: mbedtls_net_set_nonblock(net_context);
 	}
 
-	return rc;
+	return rc ? MBEDTLS_ERR_NET_INVALID_CONTEXT : 0;
 }
 
 
-int netctx_set_nodelay(netctx* ctx, int nodelay)
+mbed_err netctx_set_nodelay(netctx* ctx, int nodelay)
 {
 	int rc = INVALID_SOCKET;
 	mbedtls_net_context* const net_context = (mbedtls_net_context*)ctx;
@@ -151,7 +147,7 @@ int netctx_set_nodelay(netctx* ctx, int nodelay)
 				sizeof(tcp_nodelay));
 	}
 
-	return rc;
+	return rc ? MBEDTLS_ERR_NET_INVALID_CONTEXT : 0;
 }
 
 
@@ -182,29 +178,34 @@ int netctx_get_port(netctx* ctx)
 
 netctx_poll_status netctx_poll(netctx* ctx, netctx_poll_mode mode, uint32_t timeout)
 {
-	netctx_poll_status status = { 0, MBEDTLS_ERR_SSL_BAD_INPUT_DATA };
+	netctx_poll_status status = { NETCTX_POLL_ERROR, 0, MBEDTLS_ERR_SSL_BAD_INPUT_DATA };
 	mbedtls_net_context* const net_context = (mbedtls_net_context*)ctx;
+	uint32_t rw = 0;
 
-	if (net_context) {
-		// build the read/write flag
-		uint32_t rw = 0;
+	// build the read/write flag
+	if (mode.read == 1)
+		rw = MBEDTLS_NET_POLL_READ;
+	if (mode.write == 1)
+		rw |= MBEDTLS_NET_POLL_WRITE;
 
-		if (mode.read == 1)
-			rw = MBEDTLS_NET_POLL_READ;
-		if (mode.write == 1)
-			rw |= MBEDTLS_NET_POLL_WRITE;
-
-		int rc = mbedtls_net_poll(net_context, rw, timeout);
+	if (net_context && rw) {
+		// wait to be ready for read or write
+		const int rc = mbedtls_net_poll(net_context, rw, timeout);
 		if (rc < 0) {
+			// an error has occurred
 			status.errnum = rc;
 		}
 		else if (rc == 0) {
+			// wait timed out
+			status.status_code = NETCTX_POLL_ERROR;
 			status.errnum = MBEDTLS_ERR_SSL_TIMEOUT;
 		}
 		else {
+			// ready for read and/or write
+			status.status_code = NETCTX_POLL_OK;
 			status.errnum = 0;
-			status.poll_event.read = rc & MBEDTLS_NET_POLL_READ;
-			status.poll_event.write = rc & MBEDTLS_NET_POLL_WRITE;
+			status.event.read = rc & MBEDTLS_NET_POLL_READ;
+			status.event.write = rc & MBEDTLS_NET_POLL_WRITE;
 		}
 	}
 
