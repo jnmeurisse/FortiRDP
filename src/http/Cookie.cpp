00c52c5789890f982dfbcbe8420dec0dda9fe14c
jnmeurisse <jnmeurissegmail.com>
2024-12-31 14:16:44 +0100

    Improve session cookie management. Expires attribute is now stored in a time_t. This attribute equal -1 when the cookie is a session cookie.

--- src/http/Cookie.cpp
+++ src/http/Cookie.cpp
@@ -7,18 +7,36 @@
 */
 #include "Cookie.h"
 
+#include <chrono>
 #include <vector>
+#include <limits>
+
 #include "http/CookieError.h"
 #include "tools/StrUtil.h"
+#include "tools/strptime.h"
 
 
 namespace http {
 
-	Cookie::Cookie(const tools::obfstring& value)
+	constexpr std::time_t EXPIRES_UNSPECIFIED = -1;
+
+
+	Cookie::Cookie(
+		const std::string& name,
+		const tools::obfstring& value,
+		const std::string& domain,
+		const std::string& path,
+		const std::time_t& expires,
+		const bool secure,
+		const bool http_only) :
+		_name(name),
+		_value(value),
+		_domain(tools::lower(domain)),
+		_path(path),
+		_expires(expires < 0 ? EXPIRES_UNSPECIFIED : expires),
+		_secure(secure),
+		_http_only(http_only)
 	{
-		if (!parse(value)) {
-			throw CookieError{ "Invalid cookie value:" + value.uncrypt() };
-		}
 	}
 
 
@@ -28,9 +46,23 @@ namespace http {
 	}
 
 
-	bool Cookie::parse(const tools::obfstring& value)
+	bool Cookie::is_expired() const noexcept
 	{
-		bool rc = false;
+		using namespace std::chrono;
+
+		auto now = system_clock::now();
+		return _expires > 0 && system_clock::to_time_t(now) > _expires;
+	}
+
+
+	bool Cookie::is_session() const noexcept
+	{
+		return _expires == EXPIRES_UNSPECIFIED;
+	}
+
+	
+	Cookie Cookie::parse(const tools::obfstring& cookie_string)
+	{
 		std::vector<tools::obfstring> parts;
 
 		// Split the cookie string. There should always have one cookie-pair pair.
@@ -38,56 +70,92 @@ namespace http {
 		// to the syntax : 
 		//    cookie-pair *( ";" SP cookie-av )
 		//    cookie-pair       = cookie-name "=" cookie-value
-		if (tools::split(value, ';', parts) > 0) {
-			const tools::obfstring cookie_pair(parts[0]);
+		if (tools::split(cookie_string, ';', parts) <= 0)
+			throw CookieError{ "Empty cookie definition" };
+		
+		// split cookie-pair 
+		const tools::obfstring cookie_pair(parts[0]);
+		const std::string::size_type pos = cookie_pair.find('=');
+		if (pos == std::string::npos)
+			throw CookieError{ "Invalid cookie :" + cookie_string.uncrypt() };
 
-			// split cookie-pair 
-			const std::string::size_type pos = cookie_pair.find('=');
-			if (pos != std::string::npos) {
-				_name = cookie_pair.substr(0, pos).uncrypt();
-				_value = tools::obfstring{ cookie_pair.substr(pos + 1, std::string::npos) };
+		const std::string cookie_name{ cookie_pair.substr(0, pos).uncrypt() };
+		const tools::obfstring cookie_value{ cookie_pair.substr(pos + 1, std::string::npos) };
+		std::string domain;
+		std::string path;
+		std::time_t expires = EXPIRES_UNSPECIFIED;
+		bool secure = false;
+		bool http_only = false;
 
-				rc = true;
-			}
-			else {
-				// invalid cookie
-				rc = false;
-			}
+		if (parts.size() > 1) {
+			for (size_t idx = 1; idx < parts.size(); idx++) {
+				// get next attribute-value
+				const std::string cookie_av{ tools::trimleft(parts[idx].uncrypt()) };
+				if (cookie_av.size() > 0) {
+					// extract attribute name and value
+					const std::string::size_type pos = cookie_av.find('=');
 
-			if (rc && parts.size() > 1) {
-				for (size_t idx = 1; idx < parts.size(); idx++) {
-					// get next attribute-value
-					const std::string cookie_av{ tools::trimleft(parts[idx].uncrypt()) };
-					if (cookie_av.size() > 0) {
-						// extract attribute name and value
-						const std::string::size_type pos = cookie_av.find('=');
+					if (pos != std::string::npos) {
+						const std::string attribute_name{ cookie_av.substr(0, pos) };
+						const std::string attribute_value{ cookie_av.substr(pos + 1, std::string::npos) };
 
-						if (pos != std::string::npos) {
-							const std::string attribute_name{ cookie_av.substr(0, pos) };
-							const std::string attribute_value{ cookie_av.substr(pos + 1, std::string::npos) };
-
-							if (tools::iequal(attribute_name, "domain")) {
-								_domain = attribute_value;
-							}
-							else if (tools::iequal(attribute_name, "path")) {
-								_path = attribute_value;
-							}
-							else if (tools::iequal(attribute_name, "expires")) {
-								_expires = attribute_value;
-							}
+						if (tools::iequal(attribute_name, "domain")) {
+							domain = attribute_value;
 						}
-						else if (tools::iequal(cookie_av, "secure")) {
-							_secure = true;
+						else if (tools::iequal(attribute_name, "path")) {
+							path = attribute_value;
 						}
-						else if (tools::iequal(cookie_av, "httponly")) {
-							_http_only = true;
+						else if (tools::iequal(attribute_name, "expires")) {
+							expires = parse_http_date(attribute_value);
 						}
+						else if (tools::iequal(attribute_name, "max-age")) {
+							// not supported, attribute is ignored
+						}
+					}
+					else if (tools::iequal(cookie_av, "secure")) {
+						secure = true;
+					}
+					else if (tools::iequal(cookie_av, "httponly")) {
+						http_only = true;
 					}
 				}
 			}
 		}
 
-		return rc;
+		return Cookie(
+			cookie_name,
+			cookie_value,
+			domain,
+			path,
+			expires,
+			secure,
+			http_only
+		);
+	}
+
+
+
+	time_t Cookie::parse_http_date(const std::string& value)
+	{
+		static char* COOKIE_DATE_FORMATS[] =
+		{
+			"%a, %d %b %Y %H:%M:%S %Z",
+			"%a, %d-%b-%Y %H:%M:%S %Z"
+			"%a, %d %b %y %H:%M:%S %Z",
+			"%a, %d-%b-%y %H:%M:%S %Z"
+		};
+
+		struct tm tm;
+
+		for (unsigned int i = 0; i < std::size(COOKIE_DATE_FORMATS); i++) {
+			tm = { 0 };
+
+			char* const p = strptime(value.c_str(), COOKIE_DATE_FORMATS[i], &tm);
+			if (p && tm.tm_year > 0)
+				return mktime(&tm);
+		}
+
+		return 0;
 	}
 
 }
--- src/http/Cookie.h
+++ src/http/Cookie.h
@@ -7,6 +7,7 @@
 */
 #pragma once
 
+#include <ctime>
 #include <string>
 #include "tools/ObfuscatedString.h"
 
@@ -20,11 +21,21 @@ namespace http {
 	class Cookie final
 	{
 	public:
-		/* Allocates a cookie from a raw value. This constructor throws
-		 * a CookieError exception if the cookie can not be decoded according
-		 * to rfc6265.
+		Cookie() = delete;
+
+
+		/* Allocates a cookie from the cookie attributes.
 		*/
-		explicit Cookie(const tools::obfstring& value);
+		Cookie(
+			const std::string& name,
+			const tools::obfstring& value,
+			const std::string& domain,
+			const std::string& path,
+			const std::time_t& expires,
+			const bool secure,
+			const bool http_only
+		);
+
 
 		/* Returns an obfuscated string representation of this cookie.
 		 * The string format is compatible with Netscape specification.
@@ -47,9 +58,10 @@ namespace http {
 		*/
 		inline const std::string& get_path() const noexcept{ return _path; }
 
-		/* Returns the cookie expires attribute
+		/* Returns the cookie expires attribute.
+		*  The method returns -1 if the cookie is a session cookie.
 		*/
-		inline const std::string& get_expires() const noexcept { return _expires; }
+		inline const std::time_t& get_expires() const noexcept { return _expires; }
 
 		/* Returns true if the cookie secure attribute defined
 		*/
@@ -59,19 +71,40 @@ namespace http {
 		*/
 		inline const bool is_http_only() const noexcept { return _http_only; }
 
+		/* Returns true if the cookie is expired
+		*/
+		bool is_expired() const noexcept;
+
+		/* Returns true if it is a session cookie
+		*/
+		bool is_session() const noexcept;
+
+		/* Constructs a cookie from a Set-cookie header string.
+		*  @param cookie_string The cookie definition.
+		*  @return a Cookie.
+		*
+		*  The method throws a CookieError if the cookie is not valid.
+		*/
+		static Cookie parse(const tools::obfstring& cookie_string);
+
 	private:
-		// Parsed cookie 
+		// cookie attributes
 		std::string _name;
 		tools::obfstring _value;
 		std::string _domain;
 		std::string _path;
-		std::string _expires;
-		bool _secure = false;
-		bool _http_only = false;
+		std::time_t _expires;
+		bool _secure;
+		bool _http_only;
 
-		/* Parse a cookie into attributes
+
+		/* Convert a Cookie time representation to a time_t.
+		*
+		* @param value The string to convert to a time_t.
+		* @return The corresponding time or 0 if the string can not be parsed correctly.
+		* This expires the cookie.
 		*/
-		bool parse(const tools::obfstring& value);
+		static time_t parse_http_date(const std::string& value);
 	};
 
 }
