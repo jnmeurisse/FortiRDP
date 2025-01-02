6136b81340f8179fceec4930acb08b4ef216a640
jnmeurisse <jnmeurissegmail.com>
2025-01-02 11:16:58 +0100

    Standardize error messages.

--- src/net/Socket.cpp
+++ src/net/Socket.cpp
@@ -233,7 +233,7 @@ namespace net {
 			if (option == SocketOptions::all || option == SocketOptions::nodelay) {
 				const int tcp_nodelay = _no_delay ? 1 : 0;
 				if (setsockopt(_netctx.fd, IPPROTO_TCP, TCP_NODELAY, (const char *)&tcp_nodelay, sizeof(tcp_nodelay)) != 0) {
-					_logger->error("ERROR : set nodelay error %x on socket %x, fd=%d",
+					_logger->error("ERROR: set nodelay error %x on socket %x, fd=%d",
 						::WSAGetLastError(),
 						(uintptr_t)this,
 						_netctx.fd);
@@ -244,7 +244,7 @@ namespace net {
 
 			if (option == SocketOptions::all || option == SocketOptions::timeout) {
 				if (setsockopt(_netctx.fd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&_recv_timeout, sizeof(_recv_timeout)) != 0) {
-					_logger->error("ERROR : set receive time-out error %x on socket %x, fd=%d",
+					_logger->error("ERROR: set receive time-out error %x on socket %x, fd=%d",
 						::WSAGetLastError(),
 						(uintptr_t)this,
 						_netctx.fd);
@@ -253,7 +253,7 @@ namespace net {
 				}
 
 				if (setsockopt(_netctx.fd, SOL_SOCKET, SO_SNDTIMEO, (const char *)&_send_timeout, sizeof(_send_timeout)) != 0) {
-					_logger->error("ERROR : set send time-out error %x on socket %x, fd=%d",
+					_logger->error("ERROR: set send time-out error %x on socket %x, fd=%d",
 						::WSAGetLastError(),
 						(uintptr_t)this,
 						_netctx.fd);
