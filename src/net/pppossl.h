817a874a051346deaaa03d51b15e5780b28dea32
jnmeurisse <jnmeurissegmail.com>
2025-01-31 12:17:21 +0100

    Fix typo.

--- src/net/pppossl.h
+++ src/net/pppossl.h
@@ -34,7 +34,7 @@ extern "C" {
 	* Fortinet ppp header
 	*   header[0] = payload len + sizeof(ppp_header)
 	*   header[1] = 0x5050 (tag)
-	*   hedear[2] = payload len
+	*   header[2] = payload len
 	*/
 	typedef u16_t ppp_header[3];
 
