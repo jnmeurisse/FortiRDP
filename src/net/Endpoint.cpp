jnmeurisse/FortiRDP

commit 3a88558793c8c4939a694353f699e344b9a51cf3

Store tcp port number on an uint16_t instead of int.

src/net/Endpoint.cpp
@@ -7,6 +7,7 @@
 */
 #include "Endpoint.h"
 
+#include <limits>
 #include <stdexcept>
 #include "tools/StrUtil.h"
 
@@ -20,7 +21,7 @@ namespace net {
 	}
 
 
-	Endpoint::Endpoint(const std::string& address, const int default_port) :
+	Endpoint::Endpoint(const std::string& address, const uint16_t default_port) :
 		Endpoint()
 	{
 		init(address, default_port);
@@ -39,7 +40,7 @@ namespace net {
 	}
 
 
-	void Endpoint::init(const std::string& address, const int default_port)
+	void Endpoint::init(const std::string& address, const uint16_t default_port)
 	{
 		bool valid_port = true;
 
@@ -53,8 +54,12 @@ namespace net {
 		if ((p >= str) &&  (*p == ':')) {
 			// Extract the host name and port.
 			_hostname = tools::trim(std::string(str, p));
-			valid_port = tools::str2i((std::string(p + 1, str + address.length())), _port) &&
-							(_port > 0);
+			int port_value;
+			valid_port = tools::str2i((std::string(p + 1, str + address.length())), port_value) 
+						&& (port_value > 0) 
+						&& (port_value <= std::numeric_limits<uint16_t>::max());
+			if (valid_port)
+				_port = static_cast<uint16_t>(valid_port);
 		}
 		else {
 			_hostname = tools::trim(str);

src/net/Endpoint.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <string>
+#include <cstdint>
 
 
 namespace net {
@@ -36,7 +37,7 @@ namespace net {
 		 * if the syntax is not valid or if the port is not a valid integer.
 		 *
 		*/
-		explicit Endpoint(const std::string& address, const int default_port);
+		explicit Endpoint(const std::string& address, const uint16_t default_port);
 
 		/**
 		 * Converts this Endpoint to a printable string.
@@ -51,7 +52,7 @@ namespace net {
 		/**
 		 * Returns the port from this end point.
 		*/
-		inline const int port() const { return _port; }
+		inline const uint16_t port() const { return _port; }
 
 		/**
 		 * Returns true if this end point is undefined.
@@ -60,7 +61,7 @@ namespace net {
 
 	private:
 		std::string _hostname;
-		int _port;
+		uint16_t _port;
 
 		/**
 		 * Initializes this end point from the specified parameters.
@@ -68,7 +69,7 @@ namespace net {
 		 * @param address The endpoint address
 		 * @param default_port The default port if not specified in the address.
 		*/
-		void init(const std::string& address, int default_port);
+		void init(const std::string& address, uint16_t default_port);
 	};
 
 }

src/net/Listener.cpp
@@ -42,8 +42,8 @@ namespace net {
 		}
 
 		// Get the port that has been assigned during the bind.
-		const int bind_port = Socket::get_port();
-		if (bind_port == -1) {
+		uint16_t bind_port;
+		if (!Socket::get_port(bind_port)) {
 			_logger->error("ERROR: Listener::bind get_port error %d", WSAGetLastError());
 
 			rc = MBEDTLS_ERR_NET_BIND_FAILED;

src/net/OutputQueue.cpp
@@ -70,7 +70,7 @@ namespace net {
 
 		written = 0;
 
-		lwip_err rc = 0;
+		lwip_err rc = ERR_OK;
 
 		while (!is_empty()) {
 			// Determine the space available in the TCP send buffer.

src/net/Socket.cpp
@@ -9,6 +9,7 @@
 #include <Ws2ipdef.h>
 
 #include "Socket.h"
+#include <cstdint>
 
 
 
@@ -187,9 +188,9 @@ namespace net {
 	}
 
 
-	int Socket::get_port() const noexcept
+	bool Socket::get_port(uint16_t& port) const noexcept
 	{
-		int port = -1;
+		bool rc = false;
 
 		if (get_fd() != -1) {
 			struct sockaddr_storage sock_addr;
@@ -199,15 +200,17 @@ namespace net {
 				if (sock_addr.ss_family == AF_INET) {
 					struct sockaddr_in* addr4 = (struct sockaddr_in*)&sock_addr;
 					port = ntohs(addr4->sin_port);
+					rc = true;
 				}
 				else if (sock_addr.ss_family == AF_INET6) {
 					struct sockaddr_in6* addr6 = (struct sockaddr_in6*)&sock_addr;
 					port = ntohs(addr6->sin6_port);
+					rc = true;
 				}
 			}
 		}
 
-		return port;
+		return rc;
 	}
 
 

src/net/Socket.h
@@ -232,11 +232,10 @@ namespace net {
 		 * if the socket file descriptor is valid. It supports both IPv4 and IPv6
 		 * address families.
 		 *
-		 * @return The port number of the socket as an integer. If the socket is not
-		 *         bound or the file descriptor is invalid, the method returns -1.
+		 * @return False if the socket is not bound or the file descriptor is invalid.
 		 *
 		 */
-		int get_port() const noexcept;
+		bool get_port(uint16_t& port) const noexcept;
 
 	protected:
 		// A reference to the application logger.

src/ui/AsyncController.cpp
@@ -151,7 +151,7 @@ namespace ui {
 	}
 
 
-	bool AsyncController::create_tunnel(const net::Endpoint& remote_endpoint, int local_port,
+	bool AsyncController::create_tunnel(const net::Endpoint& remote_endpoint, uint16_t local_port,
 		bool multi_clients, bool tcp_nodelay)
 	{
 		if (_logger->is_debug_enabled()) {

src/ui/AsyncController.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <Windows.h>
+#include <cstdint>
 #include <functional>
 #include <memory>
 #include <string>
@@ -70,7 +71,7 @@ namespace ui {
 		/**
 		 * Creates a tunnel with the firewall.
 		*/
-		bool create_tunnel(const net::Endpoint& remote_endpoint, int local_port, bool multi_clients, bool tcp_nodelay);
+		bool create_tunnel(const net::Endpoint& remote_endpoint, uint16_t local_port, bool multi_clients, bool tcp_nodelay);
 
 		/**
 		 * Starts an external task. 

commit 97a981493dadfd752d46f34033dd9addc1f6362d

Fix port string to int conversion error (bug introduced in commit 3a88558)

src/net/Endpoint.cpp
@@ -59,7 +59,7 @@ namespace net {
 						&& (port_value > 0) 
 						&& (port_value <= std::numeric_limits<uint16_t>::max());
 			if (valid_port)
-				_port = static_cast<uint16_t>(valid_port);
+				_port = static_cast<uint16_t>(port_value);
 		}
 		else {
 			_hostname = tools::trim(str);