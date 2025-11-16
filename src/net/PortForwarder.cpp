jnmeurisse/FortiRDP

commit 9dedfa54a6f6cf6df63825893a7bcdf629699f5c

Remove static keyword from friend functions.

src/net/PortForwarder.cpp
@@ -318,7 +318,7 @@ namespace net {
 	}
 
 
-	static void dns_found_cb(const char *name, const ip_addr_t *ipaddr, void *callback_arg)
+	void dns_found_cb(const char *name, const ip_addr_t *ipaddr, void *callback_arg)
 	{
 		PortForwarder* const pf = (PortForwarder*)callback_arg;
 
@@ -493,7 +493,7 @@ namespace net {
 	}
 
 
-	static void timeout_cb(void* arg)
+	void timeout_cb(void* arg)
 	{
 		bool* timeout = (bool *)arg;
 

src/net/PortForwarder.h
@@ -170,13 +170,13 @@ namespace net {
 		// This callback is responsible to create a TCP socket with
 		// the remote host.  This socket is established through the secure
 		// socket.
-		friend void  dns_found_cb(const char *name, const ip_addr_t *ipaddr, void *callback_arg);
+		friend void dns_found_cb(const char *name, const ip_addr_t *ipaddr, void *callback_arg);
 
 		// A callback for successful TCP connection establishment.
 		friend err_t tcp_connected_cb(void *arg, struct tcp_pcb *tpcb, err_t err);
 
 		// A callback for handling TCP errors.
-		friend void  tcp_err_cb(void *arg, err_t err);
+		friend void tcp_err_cb(void *arg, err_t err);
 
 		// A callback for confirming data sent to the remote endpoint.  This callback
 		// is responsible for updating the count of forwarded bytes.
@@ -186,7 +186,7 @@ namespace net {
 		friend err_t tcp_recv_cb(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err);
 
 		// A callback for handling timeouts.
-		friend void  timeout_cb(void *arg);
+		friend void timeout_cb(void *arg);
 
 		// A reference to the application logger.
 		tools::Logger* const _logger;

src/net/Tunneler.cpp
@@ -362,7 +362,7 @@ namespace net {
 	}
 
 
-	static void timeout_cb(void* arg)
+	void timeout_cb(void* arg)
 	{
 		bool* timeout = (bool *)arg;
 

src/net/PPInterface.cpp
@@ -267,7 +267,7 @@ namespace net {
 	}
 
 
-	static u32_t ppp_output_cb(ppp_pcb *pcb, struct pbuf* pbuf, void *ctx)
+	u32_t ppp_output_cb(ppp_pcb *pcb, struct pbuf* pbuf, void *ctx)
 	{
 		LWIP_UNUSED_ARG(pcb);
 		net::PPInterface* const pp_interface = (net::PPInterface *)ctx;
@@ -276,7 +276,7 @@ namespace net {
 	}
 
 
-	static void	ppp_link_status_cb(ppp_pcb *pcb, int err_code, void *ctx)
+	void ppp_link_status_cb(ppp_pcb *pcb, int err_code, void *ctx)
 	{
 		LWIP_UNUSED_ARG(pcb);
 		net::PPInterface* pp_interface = (net::PPInterface *)ctx;

src/net/pppossl.c
@@ -228,6 +228,7 @@ pppossl_connect(ppp_pcb *ppp, void *ctx)
 static void
 pppossl_disconnect(ppp_pcb *ppp, void *ctx)
 {
+	LWIP_UNUSED_ARG(ctx);
 	ppp_link_end(ppp); /* notify upper layers */
 }
 
@@ -334,7 +335,8 @@ exit:
 static void
 pppossl_send_config(ppp_pcb *ppp, void *ctx, u32_t accm, int pcomp, int accomp)
 {
-	pppossl_pcb* const pppos = (pppossl_pcb *)ctx;
+	LWIP_UNUSED_ARG(ctx);
+	LWIP_UNUSED_ARG(accm);
 	LWIP_UNUSED_ARG(ppp);
 	LWIP_UNUSED_ARG(pcomp);
 	LWIP_UNUSED_ARG(accomp);
@@ -344,7 +346,8 @@ pppossl_send_config(ppp_pcb *ppp, void *ctx, u32_t accm, int pcomp, int accomp)
 static void
 pppossl_recv_config(ppp_pcb *ppp, void *ctx, u32_t accm, int pcomp, int accomp)
 {
-	pppossl_pcb* const pppos = (pppossl_pcb *)ctx;
+	LWIP_UNUSED_ARG(ctx);
+	LWIP_UNUSED_ARG(accm);
 	LWIP_UNUSED_ARG(ppp);
 	LWIP_UNUSED_ARG(pcomp);
 	LWIP_UNUSED_ARG(accomp);

src/tools/Thread.cpp
@@ -14,7 +14,7 @@
 namespace tools {
 
 	/* Internal functions */
-	static unsigned __stdcall thread_entry_point(void *);
+	unsigned __stdcall thread_entry_point(void *);
 
 	Thread::Thread(bool auto_delete) : 
 		_logger(Logger::get_logger()),

commit d1d3ce321463f7bb2da735d636a3478643866cc8

Fix timeout_cb multiple definitions link error introduced in 9dedfa5.

src/net/PortForwarder.cpp
@@ -13,6 +13,12 @@
 #include <lwip/tcp.h>
 #include "net/DnsClient.h"
 
+static void timeout_cb(void* arg)
+{
+	bool* timeout = (bool*)arg;
+	*timeout = true;
+}
+
 
 namespace net {
 
@@ -490,14 +496,6 @@ namespace net {
 		}
 
 		return rc;
-	}
-
-
-	void timeout_cb(void* arg)
-	{
-		bool* timeout = (bool *)arg;
-
-		*timeout = true;
 	}
 
 }

src/net/PortForwarder.h
@@ -185,9 +185,6 @@ namespace net {
 		// A callback for receiving data from the remote endpoint.
 		friend err_t tcp_recv_cb(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err);
 
-		// A callback for handling timeouts.
-		friend void timeout_cb(void *arg);
-
 		// A reference to the application logger.
 		tools::Logger* const _logger;
 

src/net/Tunneler.cpp
@@ -14,6 +14,13 @@
 #include "tools/ErrUtil.h"
 
 
+static void timeout_cb(void* arg)
+{
+	bool* timeout = (bool*)arg;
+	*timeout = true;
+}
+
+
 namespace net {
 
 	Tunneler::Tunneler(TlsSocket& tunnel, const Endpoint& local_ep, const Endpoint& remote_ep,
@@ -361,12 +368,6 @@ namespace net {
 		return;
 	}
 
-
-	void timeout_cb(void* arg)
-	{
-		bool* timeout = (bool *)arg;
-
-		*timeout = true;
-	}
-
 }
+
+

src/net/Tunneler.h
@@ -100,7 +100,6 @@ namespace net {
 
 	private:
 		void compute_sleep_time(timeval &timeout) const;
-		friend void  timeout_cb(void *arg);
 
 		// a reference to the application logger.
 		tools::Logger* const _logger;