8b1c660eb0f6d4e511f19bc5207a59e33c4f0a8c
jnmeurisse <jnmeurissegmail.com>
2026-01-01 13:26:21 +0100

    Fix read/write buffer logic.

--- src/net/TcpSocket.cpp
+++ src/net/TcpSocket.cpp
@@ -64,27 +64,36 @@ namespace net {
 				len
 			);
 
-		rcv_status read_status { rcv_status_code::NETCTX_RCV_ERROR, 0, 0 };
+		rcv_status read_status { rcv_status_code::NETCTX_RCV_OK, 0, 0 };
 
-		do {
-			const rcv_status rcv_data_status{ recv_data(buf, len) };
-
-			buf += rcv_data_status.rbytes;
-			len -= rcv_data_status.rbytes;
+		bool keep_reading = len > 0;
+		while (keep_reading) {
+			const rcv_status rcv_data_status =  recv_data(buf, len);
 
 			read_status.code = rcv_data_status.code;
-			read_status.rbytes += rcv_data_status.rbytes;
 			read_status.rc = rcv_data_status.rc;
 
-			if (rcv_data_status.code == rcv_status_code::NETCTX_RCV_RETRY) {
-				const poll_status poll_rcv_status = poll(rcv_data_status.rc, timer.remaining_time());
+			if (read_status.code == rcv_status_code::NETCTX_RCV_OK) {
+				// Update progress
+				buf += rcv_data_status.rbytes;
+				len -= rcv_data_status.rbytes;
+				read_status.rbytes += rcv_data_status.rbytes;
+
+				keep_reading = len > 0;
+			}
+			else if (read_status.code == rcv_status_code::NETCTX_RCV_RETRY) {
+				// Handle the "Busy/Retry" case
+				const poll_status poll_rcv_status = poll(read_status.rc, timer.remaining_time());
 
 				if (poll_rcv_status.code != poll_status_code::NETCTX_POLL_OK) {
 					read_status.code = rcv_status_code::NETCTX_RCV_ERROR;
 					read_status.rc = poll_rcv_status.rc;
+					keep_reading = false;
 				}
 			}
-		} while (len > 0 || read_status.code == rcv_status_code::NETCTX_RCV_RETRY);
+			else
+				keep_reading = false;
+		}
 
 		if (_logger->is_trace_enabled())
 			_logger->trace(
@@ -114,27 +123,36 @@ namespace net {
 				len
 			);
 
-		snd_status write_status { NETCTX_SND_ERROR, 0, 0 };
+		snd_status write_status { NETCTX_SND_OK, 0, 0 };
 
-		do {
+		bool keep_writing = len > 0;
+		while (keep_writing) {
 			const snd_status snd_data_status{ send_data(buf, len) };
 
-			buf += snd_data_status.sbytes;
-			len -= snd_data_status.sbytes;
-
 			write_status.code = snd_data_status.code;
-			write_status.sbytes += snd_data_status.sbytes;
 			write_status.rc = snd_data_status.rc;
 
-			if (snd_data_status.code == NETCTX_SND_RETRY) {
-				const poll_status poll_snd_status{ poll(snd_data_status.rc, timer.remaining_time()) };
+			if (write_status.code == snd_status_code::NETCTX_SND_OK) {
+				// Update progress
+				buf += snd_data_status.sbytes;
+				len -= snd_data_status.sbytes;
+				write_status.sbytes += snd_data_status.sbytes;
+
+				keep_writing = len > 0;
+			}
+			else if (write_status.code == NETCTX_SND_RETRY) {
+				// Handle the "Busy/Retry" case
+				const poll_status poll_snd_status{ poll(write_status.rc, timer.remaining_time()) };
 
 				if (poll_snd_status.code != poll_status_code::NETCTX_POLL_OK) {
 					write_status.code = snd_status_code::NETCTX_SND_ERROR;
 					write_status.rc = poll_snd_status.rc;
+					keep_writing = false;
 				}
 			}
-		} while (len > 0 || write_status.code == snd_status_code::NETCTX_SND_RETRY);
+			else
+				keep_writing = false;
+		}
 
 		if (_logger->is_trace_enabled())
 			_logger->trace(
